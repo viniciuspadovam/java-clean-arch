f5af0452398165449a650c5c055eecfe2dc78605
viniciuspadovam <padovam3373@gmail.com>
2025-07-09 11:36:03 -0300

    refactor: deleted nottouse packages model and repository

--- src/main/java/br/com/alura/codechella/nottouse/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.alura.codechella.nottouse.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.alura.codechella.nottouse.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
