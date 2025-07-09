106408340072c081ad228c01f423b0cd885274a7
viniciuspadovam <padovam3373@gmail.com>
2025-07-09 17:37:03 -0300

    feat: add beans

---
+++ src/main/java/br/com/alura/codechella/config/UserConfig.java
@@ -0,0 +1,30 @@
+package br.com.alura.codechella.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.alura.codechella.application.gateways.IUserRepository;
+import br.com.alura.codechella.application.usecases.CreateUser;
+import br.com.alura.codechella.infra.gateways.UserEntityMapper;
+import br.com.alura.codechella.infra.gateways.UserRepositoryJpa;
+import br.com.alura.codechella.infra.persistence.UserRepository;
+
+@Configuration
+public class UserConfig {
+
+	@Bean
+	CreateUser createUser(IUserRepository userRepository) {
+		return new CreateUser(userRepository);
+	}
+	
+	@Bean
+	UserRepositoryJpa userRepositoryJpa(UserRepository userRepository, UserEntityMapper userEntityMapper) {
+		return new UserRepositoryJpa(userRepository, userEntityMapper);
+	}
+	
+	@Bean
+	UserEntityMapper userEntityMapper() {
+		return new UserEntityMapper();
+	}
+	
+}
